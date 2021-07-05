f52eb72310c1d54fa353dc42a55d6b186022f1e0
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-07-05 17:09:05 -0400

    week 1 lab 2 task 3

---
+++ labs/week1/2nd-3-three-nums.rkt
@@ -0,0 +1,25 @@
+#lang simply-scheme
+
+; Define a procedure that takes three numbers as arguments and
+; returns the sum of the squares of the two larger numbers.
+
+(define (square n) (* n n))
+
+(define (threesum x y z)
+  (display x)
+  (display y)
+  (display z)
+  (display '-)
+  (cond ((or (> x y) (> x z)) (+ (square x) (square (if (> y z) y z))))
+        (else (+ (square y) (square z)))
+        )
+  )
+
+(threesum 3 2 1) ; x > y, y > z
+(threesum 3 1 2) ; x > y, y < z
+
+(threesum 2 1 3) ; x > y, y < z
+(threesum 2 3 1) ; x < y, y > z
+
+(threesum 1 2 3) ; x < y, y < z
+(threesum 1 3 2) ; x < y, y > z
