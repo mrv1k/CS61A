d8331861aec173548f78f4ddabafb80fb4dc01be
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2023-03-04 21:02:46 -0500

    134 2

--- js/134.js
+++ js/134.js
@@ -2,6 +2,8 @@
 
 // SICP JS 1.3.4
 
+// 1.40 - skipped - got spooked by math
+
 // 1.41
 function inc(n) {
     return n + 1;
@@ -14,6 +16,24 @@ function double(fn) {
 double(double(double))(inc)(5);
 // 2^3 = 8 of doubles = 16 + 5 = 21
 
-// skipped 1.42
+// 1.42
+function compose(f, g) {
+    return (x) => f(g(x));
+}
 
 // 1.43
+const square = (x) => x * x;
+
+
+function repeated(fn, n) {
+    return n === 0
+        ? y => y
+        : compose(fn, repeated(fn, n - 1));
+}
+
+repeated(square, 2)(5);
+// square 5 2 times
+// square 5 * 5, square 5 1 times
+// square 25 * 5, square 0 times
+// if times === 0 return 625
+
